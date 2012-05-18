34319040bd57c85fd318402c4c8fe4902bc26d64
bioinfocttj <typhaine.pl@live.fr>
2012-05-18 09:35:37 +0200

    picker modif

--- Picker.java
+++ Picker.java
@@ -14,32 +14,4 @@ abstract public class Picker {
 	protected static String cropMode;
 	protected static String noiseT;
 	protected static boolean cropperMode;
-	
-	/*static double[][] resultConverter() {
-		int arrayLength = xtab.size();
-		Object[] tempX = new String[arrayLength];
-		Object[] tempY = new String[arrayLength];
-		Object[] tempZ = new String[arrayLength];
-		tempX = xtab.toArray();
-		tempY = ytab.toArray();
-		tempZ = slice.toArray();
-		double[] xArray = new double[arrayLength];
-		double[] yArray = new double[arrayLength];
-		double[] zArray = new double[arrayLength];
-		for (int i=0; i < arrayLength; i++){
-			String temp = String.valueOf(tempX[i]);
-			xArray[i] = Double.parseDouble(temp);
-			temp = String.valueOf(tempY[i]);
-			yArray[i] = Double.parseDouble(temp);
-			temp = String.valueOf(tempZ[i]);
-			zArray[i] = Double.parseDouble(temp);
-		}
-		double[][] coordinates = new double[3][arrayLength];
-		coordinates[0]=xArray;
-		coordinates[1]=yArray;
-		coordinates[2]=zArray;
-		return coordinates;
-	}*/
-		
-
 }
